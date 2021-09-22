bc00061616208f94650d0b30cccfb8f7a4d31fb2
RafsanRifat <rafsan.rifat255@gmail.com>
2021-09-22 11:55:40 +0600

    templates rendered

--- Django_practice/settings.py
+++ Django_practice/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Django_practice.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

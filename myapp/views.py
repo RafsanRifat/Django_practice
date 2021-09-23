8b8acc2d9f24545d7a1538f9b3ebf5145bf504fa
RafsanRifat <rafsan.rifat255@gmail.com>
2021-09-23 13:20:14 +0600

    views added for contact form post method

--- myapp/views.py
+++ myapp/views.py
@@ -12,4 +12,11 @@ def home(request):
 
 
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        text = request.POST['text']
+    print(name)
+    print(email)
+    print(text)
     return render(request, 'contact.html')

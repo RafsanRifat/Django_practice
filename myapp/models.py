93cb006f1c2cb01791776d6f5f7f5134b97ba13a
RafsanRifat <rafsan.rifat255@gmail.com>
2021-09-27 16:20:50 +0600

    obj name change in database

--- myapp/models.py
+++ myapp/models.py
@@ -6,3 +6,6 @@ class Contact(models.Model):
     name = models.CharField(max_length=50)
     email = models.CharField(max_length=100)
     text = models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.name

62a8ff1d053e7c7ede49cf0b08b799c5d1128721
RafsanRifat <rafsan.rifat255@gmail.com>
2021-09-29 16:09:59 +0600

    fields added to Contact form

--- myapp/forms.py
+++ myapp/forms.py
@@ -3,6 +3,9 @@ from  .models import Contact
 
 
 class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = '__all__'
+    # class Meta:
+    #     model = Contact
+    #     fields = '__all__'
+    name = forms.CharField(max_length=100, label='Your Name:')
+    number = forms.CharField(max_length=18, label='Your Phone Number')
+    text = forms.CharField(max_length=500, label='About You:')

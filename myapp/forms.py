046384368fe88367d1b9e9e479385db495cfa742
RafsanRifat <rafsan.rifat255@gmail.com>
2021-10-02 11:54:45 +0600

    data sending to database

--- myapp/forms.py
+++ myapp/forms.py
@@ -2,10 +2,10 @@ from django import forms
 from .models import Contact
 
 
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = '__all__'
-    # name = forms.CharField(max_length=100, label='Your Name:')
-    # number = forms.CharField(max_length=18, label='Your Phone Number')
-    # text = forms.CharField(max_length=500, label='About You:')
+class ContactForm(forms.Form):
+    # class Meta:
+    #     model = Contact
+    #     fields = '__all__'
+    name = forms.CharField(max_length=100, label='Your Name:')
+    email = forms.CharField(max_length=18, label='Your Phone Number')
+    text = forms.CharField(max_length=500, label='About You:')

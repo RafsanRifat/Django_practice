43cf7b152d67784c9da90a2053022de8822e6295
RafsanRifat <rafsan.rifat255@gmail.com>
2021-10-02 15:47:54 +0600

    crispy form added but not working

--- myapp/views.py
+++ myapp/views.py
@@ -18,11 +18,12 @@ def contact(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            name = form.cleaned_data['name']
-            email = form.cleaned_data['email']
-            text = form.cleaned_data['text']
-            obj = Contact(name=name, email=email, text=text)
-            obj.save()
+            # name = form.cleaned_data['name']
+            # email = form.cleaned_data['email']
+            # text = form.cleaned_data['text']
+            # obj = Contact(name=name, email=email, text=text)
+            # obj.save()    # django form er khetre code gulo dorkar, r Model form use korle code gulo use na kore form.save() diyeai kaj hbe
+            form.save()     # uporer code gulo na use kore eita use korleo form data save hobe database a
     else:
         form = ContactForm()
     contex = {'form': form}

1f5b55726482b91457b2fc3c806b3e693e4cac9e
RafsanRifat <rafsan.rifat255@gmail.com>
2021-10-06 18:33:24 +0600

    subject and class_in models created in models.py

--- myapp/models.py
+++ myapp/models.py
@@ -6,13 +6,26 @@ from multiselectfield import MultiSelectField
 from django.contrib.auth.models import User
 
 
-
 # Create your models here.
 
 class Contact(models.Model):
     name = models.CharField(max_length=50)
     email = models.CharField(max_length=100)
     text = models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Class_in(models.Model):
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
@@ -40,17 +53,16 @@ class Post(models.Model):
     category = models.CharField(max_length=100, choices=CATEGORY)
     created_at = models.DateTimeField(default=now)
     image = models.ImageField(default='/myapp/images/default.jpg', upload_to='myapp/images')
-    medium = MultiSelectField(max_length=150, choices=MEDIUM, max_choices = 5, default='Bangla')
+    medium = MultiSelectField(max_length=150, choices=MEDIUM, max_choices=5, default='Bangla')
 
     def save(self):
         self.slug = slugify(self.title)
-        super().save()    #saving image first
+        super().save()  # saving image first
         img = Image.open(self.image.path)
         if img.height > 300 or img.width > 300:
             output_size = (500, 320)
-            img.thumbnail (output_size)
+            img.thumbnail(output_size)
             img.save(self.image.path)
-
 
     def __str__(self):
         return self.title

9187288cbaa7c3b8d57b1718c018abbc1dbd42f6
RafsanRifat <rafsan.rifat255@gmail.com>
2021-10-10 17:08:41 +0600

    PostListView url added

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,4 +1,4 @@
-from .views import home,contact,post,postcreate
+from .views import home,contact,post,postcreate,PostListView
 from django.urls import path,include
 
 urlpatterns = [
@@ -6,4 +6,5 @@ urlpatterns = [
     path('contact/', contact, name='contact'),
     path('post/', post, name='post'),
     path('postcreate/', postcreate, name='postcreate'),
+    path('postlist/', PostListView, name='postlistview'),
 ]

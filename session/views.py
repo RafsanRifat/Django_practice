fc7ea45e974c039b4686824820316d39e1c1c07d
RafsanRifat <rafsan.rifat255@gmail.com>
2021-10-23 18:03:30 +0600

    invalid user or password message added

--- session/views.py
+++ session/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 # Create your views here.
@@ -9,7 +10,13 @@ from django.contrib.auth.models import User
 def loginuser(request):
     if request.method == 'POST':
         form = AuthenticationForm(request = request, data = request.POST)
-        if form.is_valid()():
+        if form.is_valid():
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
+            if user is not None:
+                login(user)
+                return redirect('home')
+            else:
+                messages.error(request, 'invalid User name or Password')
+
     return render(request, 'session/login.html')

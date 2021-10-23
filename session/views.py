a7ed41a54e57628f4ab5e48f0efc0bbbcbdf556b
RafsanRifat <rafsan.rifat255@gmail.com>
2021-10-23 17:37:48 +0600

    123

--- session/views.py
+++ session/views.py
@@ -7,4 +7,9 @@ from django.contrib.auth.models import User
 # Create your views here.
 
 def loginuser(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request = request, data = request.POST)
+        if form.is_valid()():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
     return render(request, 'session/login.html')

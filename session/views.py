0cb1c00edf669aade72c14358bf5c1fbc0161329
RafsanRifat <rafsan.rifat255@gmail.com>
2021-10-26 17:08:54 +0600

    email functionality added to registration view

--- session/views.py
+++ session/views.py
@@ -4,20 +4,22 @@ from django.contrib.auth import authenticate, login, logout, update_session_auth
 from django.contrib.auth.models import User
 from django.contrib import messages
 from .forms import SignUpForm
+
 """ For email """
 from django.contrib.sites.shortcuts import get_current_site
 from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
 
+
 # Create your views here.
 
 def loginuser(request):
     if request.method == 'POST':
-        form = AuthenticationForm(request = request, data = request.POST)
+        form = AuthenticationForm(request=request, data=request.POST)
         if form.is_valid():
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
-            user = authenticate(username = username, password = password)
+            user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
                 return redirect('home')
@@ -30,7 +32,6 @@ def loginuser(request):
         return render(request, 'session/login.html', {'form': form})
 
 
-
 def logoutuser(request):
     logout(request)
     messages.success(request, "Successfully loged out")
@@ -41,10 +42,17 @@ def registration(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
-           user = form.save()
-           current_site = get_current_site(request)
-           mail_subject = 'Account created'
-           
+            """ Email functionality added """
+            user = form.save()
+            current_site = get_current_site(request)
+            mail_subject = 'Account created'
+            messages = render_to_string('session/account.html', {
+                'user': user,
+                'domain': current_site.domain
+            })
+            send_mail = form.cleaned_data.get('email')
+            email = EmailMessage(mail_subject, messages, to=[send_mail])
+            email.send()
             return redirect('login')
     else:
         form = SignUpForm
